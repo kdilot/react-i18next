30ee83129925c0b8935e78f721b4eacaba2ef634
kdilot <kdilotdev@gmail.com>
2020-04-03 14:49:13 +0900

    Updates

---
+++ precache-manifest.febdd332f33d55de764d24fdc390a842.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b6933be42882f092ccf58cc3d3a1796",
+    "url": "/react-i18next/index.html"
+  },
+  {
+    "revision": "18f7cf8047a73e397cce",
+    "url": "/react-i18next/static/css/main.5383ba55.chunk.css"
+  },
+  {
+    "revision": "3e7bee8cb2c874b34301",
+    "url": "/react-i18next/static/js/2.f8fe5f93.chunk.js"
+  },
+  {
+    "revision": "b92b369c485a56d25a5a7495ecf46b59",
+    "url": "/react-i18next/static/js/2.f8fe5f93.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "18f7cf8047a73e397cce",
+    "url": "/react-i18next/static/js/main.997e2591.chunk.js"
+  },
+  {
+    "revision": "d657f2d9ceacd0cce6d5",
+    "url": "/react-i18next/static/js/runtime-main.d6c13546.js"
+  }
+]);
